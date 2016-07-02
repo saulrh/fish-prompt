49a687515e84ea1021a4446cc0bb41315f2426dd
Saul Reynolds-Haertle <saulrh@gatech.edu>
2016-07-02 19:40:59 -0400

    commit

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,2 +1,9 @@
+
 function fish_prompt
+    echo "["(tint: green (whoami))"@"(tint: green (hostname)) (dirname (pwd))/(tint: 27f (basename (pwd)))"]"
+    echo "[ "
 end
+
+# Local Variables:
+# mode: sh
+# End:
